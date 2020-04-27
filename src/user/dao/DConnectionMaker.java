73a2a48ae467a13784883ee9cf9ac58d34e64a21
JY <kims2606@gmail.com>
2020-04-27 15:06:17 +0900

    74 ~ 77 P. Connection 생성 클래스를 인터페이스로 추상화.

--- src/user/dao/DConnectionMaker.java
+++ src/user/dao/DConnectionMaker.java
@@ -4,9 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
+public class DConnectionMaker implements ConnectionMaker {
 
     public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+        //D 사의 독자적인 방법으로 Connection을 생성하는 코드가 들어감.
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost/toby_spring_learning?serverTimezone=UTC", "root", "1234");
     }
