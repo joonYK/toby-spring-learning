03bf281ad12b18e0a8ad6c1a8592636b396b5274
JY <kims2606@gmail.com>
2020-06-02 11:39:53 +0900

    157 ~ 159 P. UserDaoTest Junit 테스트

--- src/user/dao/DConnectionMaker.java
+++ src/user/dao/DConnectionMaker.java
@@ -8,7 +8,7 @@ public class DConnectionMaker implements ConnectionMaker {
 
     public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
         //D 사의 독자적인 방법으로 Connection을 생성하는 코드가 들어감.
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost/toby_spring_learning?serverTimezone=UTC", "root", "1234");
     }
 
--- src/user/dao/UserDaoTest.java
+++ src/user/dao/UserDaoTest.java
@@ -1,5 +1,9 @@
 package user.dao;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import user.domain.User;
@@ -8,9 +12,9 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
 
-    public static void main(String[] args) throws SQLException {
+    @Test
+    public void addAndGet() throws SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        //ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
@@ -20,16 +24,10 @@ public class UserDaoTest {
 
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
-
         User user2 = dao.get(user.getId());
-        if(!user.getName().equals(user2.getName()))
-            System.out.println("테스트 실패 (name)");
-        else if(!user.getPassword().equals(user2.getPassword()))
-            System.out.println("테스트 실패 (password)");
-        else
-            System.out.println(user2.getId() + " 조회 테스트 성공");
 
+        Assert.assertThat(user2.getName(), CoreMatchers.is(user.getName()));
+        Assert.assertThat(user2.getPassword(), CoreMatchers.is(user.getPassword()));
     }
 
 }
