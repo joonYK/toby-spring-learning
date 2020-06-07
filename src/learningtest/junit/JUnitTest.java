965f93d4b087f096386eebc1166fe3e8106d230f
JY <shutingz@naver.com>
2020-06-07 22:28:58 +0900

    201 ~ 202 P. JUnit 학습테스트 개선.

--- src/learningtest/junit/JUnitTest.java
+++ src/learningtest/junit/JUnitTest.java
@@ -3,27 +3,31 @@ package learningtest.junit;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.not;
 
 public class JUnitTest {
-    static JUnitTest testObject;
+    static List<JUnitTest> testObjects = new ArrayList<>();
 
     @Test
     public void test1() {
-        Assert.assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        Assert.assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test2() {
-        Assert.assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        Assert.assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test3() {
-        Assert.assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        Assert.assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
 }
