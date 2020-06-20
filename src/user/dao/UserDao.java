cd6083e5c7c386f215679266b2068d9f18f90653
JY <shutingz@naver.com>
2020-06-20 12:14:19 +0900

    308 ~ 310 P. UserDao를 인터페이스로 적용.

--- src/user/dao/UserDao.java
+++ src/user/dao/UserDao.java
@@ -1,79 +1,13 @@
 package user.dao;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
-import user.dao.exception.DuplicateUserIdException;
 import user.domain.User;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-public class UserDao {
-
-    private JdbcTemplate jdbcTemplate;
-
-    private RowMapper<User> userRowMapper = new RowMapper<User>() {
-        @Override
-        public User mapRow(ResultSet resultSet, int i) throws SQLException {
-            User user = new User();
-            user.setId(resultSet.getString("id"));
-            user.setName(resultSet.getString("name"));
-            user.setPassword(resultSet.getString("password"));
-
-            return user;
-        }
-    };
-
-    public void setDataSource(DataSource dataSource) {
-        jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
-    public void add(final User user) {
-        try {
-            jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
-                    user.getId(), user.getName(), user.getPassword());
-        } catch (DuplicateKeyException e) {
-            throw new DuplicateUserIdException(e);
-        }
-    }
-
-    public User get(String id) throws EmptyResultDataAccessException {
-        return jdbcTemplate.queryForObject("select * from users where id = ?",
-                new Object[]{id}, userRowMapper);
-    }
-
-    public List<User> getAll() {
-        return jdbcTemplate.query("select * from users order by id",
-                userRowMapper);
-    }
-
-    public void deleteAll() {
-        jdbcTemplate.update("delete from users");
-    }
-
-    public int getCount() {
-        return jdbcTemplate.query(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                return connection.prepareStatement("select count(*) from users");
-            }
-        }, new ResultSetExtractor<Integer>() {
-            @Override
-            public Integer extractData(ResultSet resultSet) throws SQLException, DataAccessException {
-                resultSet.next();
-                return resultSet.getInt(1);
-            }
-        });
-
-    }
-
+public interface UserDao {
+    void add(User user);
+    User get(String id);
+    List<User> getAll();
+    void deleteAll();
+    int getCount();
 }
