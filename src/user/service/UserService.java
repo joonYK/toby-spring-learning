180f38b60b6e2993c9b70fa7dd25b5e4575caf01
JY <shutingz@naver.com>
2020-06-22 22:28:30 +0900

    332 ~ 334 P. UserService upgradeLevels 메서드 추가.

--- src/user/service/UserService.java
+++ src/user/service/UserService.java
@@ -1,14 +1,37 @@
 package user.service;
 
 import user.dao.UserDao;
+import user.domain.Level;
+import user.domain.User;
+
+import java.util.List;
 
 public class UserService {
 
-    UserDao userDao;
+    private UserDao userDao;
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
 
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            boolean changed;
+
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if(user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            } else {
+                changed = false;
+            }
+
+            if(changed)
+                userDao.update(user);
+        }
+    }
 
 }
--- src/user/service/UserServiceTest.java
+++ src/user/service/UserServiceTest.java
@@ -1,11 +1,18 @@
 package user.service;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import user.dao.UserDao;
+import user.domain.Level;
+import user.domain.User;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/test-applicationContext.xml")
@@ -14,8 +21,41 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserDao userDao;
+
+    private List<User> users;
+
+    @Before
+    public void setUp() {
+        users = Arrays.asList(
+                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
+                new User("erwins", "박범진", "p3", Level.SILVER, 60, 29),
+                new User("madnite1", "박범진", "p4", Level.SILVER, 60, 30),
+                new User("green", "박범진", "p5", Level.GOLD, 100, 100)
+        );
+    }
+
     @Test
-    public void bean() {
-        Assert.assertNotNull(userService);
+    public void upgradeLevels() {
+        userDao.deleteAll();
+
+        for (User user : users)
+            userDao.add(user);
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        Assert.assertEquals(userUpdate.getLevel(), expectedLevel);
     }
 }
