7953a3d9be1699f2f0543dbd8fd9ee3a690abba0
JY <shutingz@naver.com>
2020-06-23 22:35:53 +0900

    340 ~ 342 P. UserService upgradeLevel 메서드 리팩토링.

--- src/user/domain/Level.java
+++ src/user/domain/Level.java
@@ -1,20 +1,31 @@
 package user.domain;
 
 public enum Level {
-    BASIC(1),
+    BASIC(1, 2),
 
-    SILVER(2),
+    SILVER(2, 3),
 
-    GOLD(3);
+    GOLD(3, null);
 
     private final int value;
+
+    //다음 단계로 등급이 올라갈때의 레벨.
+    private final Integer nextLevelValue;
 
     public int intValue() {
         return value;
     }
 
-    Level(int value) {
+    public Level nextLevel() {
+        if(nextLevelValue == null)
+            return null;
+
+        return valueOf(nextLevelValue);
+    }
+
+    Level(int value, Integer nextLevelValue) {
         this.value = value;
+        this.nextLevelValue = nextLevelValue;
     }
 
     public static Level valueOf(int value) {
