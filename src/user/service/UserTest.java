6032718c9595718d9a1b1329e9ea4e974714903b
JY <kims2606@gmail.com>
2020-06-24 09:05:25 +0900

    344 ~ 345 P. User 테스트 추가.

---
+++ src/user/service/UserTest.java
@@ -0,0 +1,41 @@
+package user.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import user.domain.Level;
+import user.domain.User;
+
+public class UserTest {
+    private User user;
+
+    @Before
+    public void setUp() {
+        user = new User();
+    }
+
+    @Test
+    public void upgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() == null)
+                continue;
+
+            user.setLevel(level);
+            user.upgradeLevel();
+            Assert.assertEquals(user.getLevel(), level.nextLevel());
+        }
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void cannotUpgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() != null)
+                continue;
+
+            user.setLevel(level);
+            user.upgradeLevel();
+        }
+    }
+}
