d00ed2a7501740dfaaff0677ca92bd433f36aeba
JY <shutingz@naver.com>
2020-06-27 18:01:19 +0900

    6장 418 ~ 420 P. (6.2 고립된 단위 테스트) UserServiceTest 클래스에 UserDao 목 오브젝트 추가.

--- src/user/service/UserServiceTest.java
+++ src/user/service/UserServiceTest.java
@@ -65,6 +65,41 @@ public class UserServiceTest {
     static class TestUserServiceException extends RuntimeException {
     }
 
+    static class MockUserDao implements UserDao {
+        private List<User> users;
+        private List<User> updated = new ArrayList<>();
+
+        public MockUserDao(List<User> users) {
+            this.users = users;
+        }
+
+        public List<User> getUpdated() {
+            return updated;
+        }
+
+        @Override
+        public List<User> getAll() {
+            return users;
+        }
+
+        @Override
+        public void update(User user) {
+            updated.add(user);
+        }
+
+        @Override
+        public void add(User user) { throw new UnsupportedOperationException(); }
+
+        @Override
+        public User get(String id) { throw new UnsupportedOperationException(); }
+
+        @Override
+        public void deleteAll() {throw new UnsupportedOperationException();}
+
+        @Override
+        public int getCount() {throw new UnsupportedOperationException();}
+    }
+
     @Autowired
     private UserService userService;
 
