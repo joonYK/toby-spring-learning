ac68a9b949ff2a4c712f0aca2852fd35c2e39a7c
JY <kims2606@gmail.com>
2020-07-01 12:40:09 +0900

    6장 444 ~ 446 P.(6.3 다이내믹 프록시와 팩토리 빈) 다이내믹 프록시 확장.

--- src/learningtest/jdk/UppercaseHandler.java
+++ src/learningtest/jdk/UppercaseHandler.java
@@ -4,15 +4,22 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
-    private Hello target;
+    //어떤 종류의 타깃도 적용가능하도록 Object 사용.
+    private Object target;
 
-    public UppercaseHandler(Hello target) {
+    public UppercaseHandler(Object target) {
         this.target = target;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String ret = (String)method.invoke(target, args);
-        return ret.toUpperCase();
+
+        Object ret = method.invoke(target, args);
+
+        //메서드의 결과가 String 이면서 메소드의 이름이 say로 시작하는 경우만 대문자로 반환.
+        if(ret instanceof String && method.getName().startsWith("say"))
+            return ((String)ret).toUpperCase();
+        else
+            return ret;
     }
 }
