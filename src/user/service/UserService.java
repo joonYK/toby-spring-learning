99274d154362a2fdaacde87b67518dd19b9cacd7
JY <shutingz@naver.com>
2020-07-11 23:00:45 +0900

    6장 536 ~ 538 P. (6.6 트랜잭션 속성) 트랜잭션 애노테이션으로 트랜잭션 설정

--- src/user/service/UserService.java
+++ src/user/service/UserService.java
@@ -1,15 +1,20 @@
 package user.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import user.domain.User;
 
 import java.util.List;
 
+@Transactional //<tx:method name="*" />과 같은 설정 효과
 public interface UserService {
     void add(User user);
-    User get(String id);
-    List<User> getAll();
     void deleteAll();
     void update(User user);
-
     void upgradeLevels();
+
+    @Transactional(readOnly = true)
+    User get(String id);
+
+    @Transactional(readOnly = true)
+    List<User> getAll();
 }
