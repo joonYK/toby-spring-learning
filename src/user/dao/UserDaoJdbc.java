180adab9004357e7df44a194f2b286afeec111a6
JY <shutingz@naver.com>
2020-07-12 16:01:43 +0900

    7장 559 ~ 560 P. (7.1 SQL과 DAO의 분리) XML 설정을 사용해서 분리.

--- src/user/dao/UserDaoJdbc.java
+++ src/user/dao/UserDaoJdbc.java
@@ -18,6 +18,12 @@ import java.util.List;
 
 public class UserDaoJdbc implements UserDao {
 
+    private String sqlAdd;
+
+    public void setSqlAdd(String sqlAdd) {
+        this.sqlAdd = sqlAdd;
+    }
+
     private JdbcTemplate jdbcTemplate;
 
     private RowMapper<User> userRowMapper = new RowMapper<User>() {
@@ -41,8 +47,9 @@ public class UserDaoJdbc implements UserDao {
     }
 
     public void add(final User user) {
-        jdbcTemplate.update("insert into users(id, name, password, level, login ,recommend, email) values(?,?,?,?,?,?,?)",
-                user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail());
+        jdbcTemplate.update(sqlAdd,
+                user.getId(), user.getName(), user.getPassword(), user.getEmail(),
+                user.getLevel().intValue(), user.getLogin(), user.getRecommend());
 
     }
 
