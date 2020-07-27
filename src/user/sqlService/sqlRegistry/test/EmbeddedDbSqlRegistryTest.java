5c903b06d28b1f08fc894f6e17cc4a1704680a7a
JY <shutingz@naver.com>
2020-07-27 21:59:14 +0900

    7장 634 ~ 639 P. (7.5 DI를 이용해 다양한 구현 방법 적용) 내장형 DB를 사용하는 SqlRegistry 생성 및 테스트. (테스트는 팩토리 메서드 패턴 적용.)

---
+++ src/user/sqlService/sqlRegistry/test/EmbeddedDbSqlRegistryTest.java
@@ -0,0 +1,31 @@
+package user.sqlService.sqlRegistry.test;
+
+import org.junit.After;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import user.sqlService.sqlRegistry.EmbeddedDbSqlRegistry;
+import user.sqlService.sqlRegistry.UpdatableSqlRegistry;
+
+public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
+
+    EmbeddedDatabase db;
+
+    @Override
+    protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+        db = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("user/sqlService/sqlRegistry/sqlRegistrySchema.sql")
+                .build();
+
+        EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
+        embeddedDbSqlRegistry.setDataSource(db);
+
+        return embeddedDbSqlRegistry;
+    }
+
+    @After
+    public void tearDown() {
+        db.shutdown();
+    }
+}
