57bf7b99755e0ca2def925125a8e3240c4130c78
JY <shutingz@naver.com>
2020-08-06 21:17:21 +0900

    7장 658 ~ 660 P. (7.6 스프링 3.1의 DI) DataSource 빈 생성을 자바코드로 변경. (XML -> @Bean)

--- src/user/TestApplicationContext.java
+++ src/user/TestApplicationContext.java
@@ -1,9 +1,28 @@
 package user;
 
+import com.mysql.cj.jdbc.Driver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.test.context.ContextConfiguration;
+
+import javax.sql.DataSource;
 
 @ContextConfiguration
 @ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/testdb?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+
+        return dataSource;
+    }
+
+
+
 }
