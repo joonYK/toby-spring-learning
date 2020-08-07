96afa0e84a4be69f2320e957d040d7e49b69af2c
JY <kims2606@gmail.com>
2020-08-07 10:22:26 +0900

    7장 665 ~ 671 P. (7.6 스프링 3.1의 DI) bean 생성을 자바 코드로 변경.

--- src/user/TestApplicationContext.java
+++ src/user/TestApplicationContext.java
@@ -1,28 +1,34 @@
 package user;
 
 import com.mysql.cj.jdbc.Driver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.mail.MailSender;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import user.dao.UserDao;
 import user.dao.UserDaoJdbc;
 import user.service.DummyMailSender;
 import user.service.UserService;
 import user.service.UserServiceImpl;
 import user.service.UserServiceTest;
+import user.sqlService.OxmSqlService;
 import user.sqlService.SqlService;
+import user.sqlService.sqlRegistry.EmbeddedDbSqlRegistry;
+import user.sqlService.sqlRegistry.SqlRegistry;
 
 import javax.sql.DataSource;
 
 @ContextConfiguration
-@ImportResource("/test-applicationContext.xml")
+@EnableTransactionManagement
+//@ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
-
-    @Autowired
-    SqlService sqlService;
 
     @Bean
     public DataSource dataSource() {
@@ -36,10 +42,17 @@ public class TestApplicationContext {
     }
 
     @Bean
+    public PlatformTransactionManager transactionManager() {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource());
+        return transactionManager;
+    }
+
+    @Bean
     public UserDao userDao() {
         UserDaoJdbc dao = new UserDaoJdbc();
         dao.setDataSource(dataSource());
-        dao.setSqlService(sqlService);
+        dao.setSqlService(sqlService());
         return dao;
     }
 
@@ -53,9 +66,7 @@ public class TestApplicationContext {
 
     @Bean
     public MailSender mailSender() {
-        DummyMailSender mailSender = new DummyMailSender();
-        mailSender.setHost("mail.server.com");
-        return mailSender;
+         return new DummyMailSender();
     }
 
     @Bean
@@ -65,6 +76,37 @@ public class TestApplicationContext {
 
         return testUserService;
 
+    }
+
+    @Bean
+    public SqlService sqlService() {
+        OxmSqlService sqlService = new OxmSqlService();
+        sqlService.setUnmarshaller(unmarshaller());
+        sqlService.setSqlRegistry(sqlRegistry());
+        return sqlService;
+    }
+
+    @Bean
+    public SqlRegistry sqlRegistry() {
+        EmbeddedDbSqlRegistry sqlRegistry = new EmbeddedDbSqlRegistry();
+        sqlRegistry.setDataSource(embeddedDatabase());
+        return sqlRegistry;
+    }
+
+    @Bean
+    public Unmarshaller unmarshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("user.sqlService.jaxb");
+        return marshaller;
+    }
+
+    @Bean
+    public DataSource embeddedDatabase() {
+        return new EmbeddedDatabaseBuilder()
+                .setName("embeddedDatabase")
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("user/sqlService/sqlRegistry/sqlRegistrySchema.sql")
+                .build();
     }
 
 
